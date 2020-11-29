43315b652ed190445b91dc37f3209de2706c407e
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2020-11-29 00:03:10 -0600

    Añadiendo btn reiniciar y enviar

--- app/src/main/java/com/example/juego21/MainActivity.java
+++ app/src/main/java/com/example/juego21/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.juego21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,14 +18,19 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
-    TextView total,solo;
-    Button addCarta;
+    TextView total;
+    Button addCarta,limpiarLinear;
     Integer puntos=0;
     LinearLayout layout1;
     RequestQueue queue;
@@ -33,6 +41,8 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.addCarta).setOnClickListener(this);
+        findViewById(R.id.btn_borrar).setOnClickListener(this);
+        findViewById(R.id.btn_enviar_puntos).setOnClickListener(this);
         layout1 = findViewById(R.id.layout1);
 
        /* addCarta.setOnClickListener(new View.OnClickListener() {
@@ -45,14 +55,17 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
             }
         });*/
 
+
+
+
        queue = Volley.newRequestQueue(this);
     }
 
     @Override
     public void onClick(View view) {
 
-        switch (view.getId()){
-            case R.id.addCarta:
+        switch (view.getId()) {
+            case R.id.addCarta: //OBTENER NUMERO
 
                 String url = "https://ramiro174.com/api/numero";
 
@@ -61,29 +74,27 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
                     public void onResponse(JSONObject response) {
 
                         try {
-
-                            solo = findViewById(R.id.text_puntos);
                             total = findViewById(R.id.text_total);
                             ImageView imageView = new ImageView(MainActivity.this);
                             Integer numero = response.getInt("numero");
 
-                            switch (numero){
-                                case 1: imageView.setImageResource(R.drawable.uno_c); break;
-                                case 2: imageView.setImageResource(R.drawable.dos_c); break;
-                                case 3: imageView.setImageResource(R.drawable.tres_c); break;
-                                case 4: imageView.setImageResource(R.drawable.cuatro_c); break;
-                                case 5: imageView.setImageResource(R.drawable.cinco_c); break;
-                                case 6: imageView.setImageResource(R.drawable.seis_c); break;
-                                case 7: imageView.setImageResource(R.drawable.siete_c); break;
-                                case 8: imageView.setImageResource(R.drawable.ocho_c); break;
-                                case 9: imageView.setImageResource(R.drawable.nueve_c); break;
-                                case 10: imageView.setImageResource(R.drawable.diez_c); break;
-                                case 11: imageView.setImageResource(R.drawable.once_c); break;
-                                case 12: imageView.setImageResource(R.drawable.doce_c); break;
-                                case 13: imageView.setImageResource(R.drawable.trese_c); break;
+                            switch (numero) {
+                                case 1: imageView.setImageResource(R.drawable.uno_c);break;
+                                case 2: imageView.setImageResource(R.drawable.dos_c);break;
+                                case 3: imageView.setImageResource(R.drawable.tres_c);break;
+                                case 4: imageView.setImageResource(R.drawable.cuatro_c);break;
+                                case 5: imageView.setImageResource(R.drawable.cinco_c);break;
+                                case 6: imageView.setImageResource(R.drawable.seis_c);break;
+                                case 7: imageView.setImageResource(R.drawable.siete_c);break;
+                                case 8: imageView.setImageResource(R.drawable.ocho_c);break;
+                                case 9: imageView.setImageResource(R.drawable.nueve_c);break;
+                                case 10: imageView.setImageResource(R.drawable.diez_c);break;
+                                case 11: imageView.setImageResource(R.drawable.once_c);break;
+                                case 12: imageView.setImageResource(R.drawable.doce_c);break;
+                                case 13: imageView.setImageResource(R.drawable.trese_c);break;
                             }
                             puntos += numero;
-                            addView(imageView,0,LinearLayout.LayoutParams.MATCH_PARENT);
+                            addView(imageView, 0, LinearLayout.LayoutParams.MATCH_PARENT);
                             total.setText(puntos.toString());
 
                         } catch (JSONException e) {
@@ -99,10 +110,45 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
                 });
                 queue.add(obtenerNumero);
                 break;
-            case 2:
+            case R.id.btn_borrar: //REINICIAR JUEGO
+                puntos = 0;
+                total.setText("");
+                if (layout1.getChildCount() > 0) {
+                    layout1.removeAllViews();
+                }
                 break;
+            case R.id.btn_enviar_puntos:
+                String urlend = "https://ramiro174.com/api/enviar/numero";
+                JSONObject datos = new JSONObject();
+
+                try {
+                    datos.put("nombre", "JuanVaz");
+                    datos.put("numero", puntos);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, urlend, datos, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                        Log.i("Respuesta:", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+                queue.add(postRequest);
+                break;
+            case R.id.btn_resultados:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(("https://ramiro174.com/resultados"))));
+                break;
+            }
+
         }
-    }
+
 
     public void addView(ImageView imageView, int width, int height){
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width,height,1);
